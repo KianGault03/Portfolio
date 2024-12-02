d34d1d84bf0601f73b31cfbe51da5cd61cc6414e
KianGault03 <Kian.Gault@outlook.com>
2024-12-02 13:47:55 +0000

    Add files via upload

---
+++ ASP.NET Project/ASP.NET_SourceFiles/Repositories/IAmbulanceRecordRepository.cs
@@ -0,0 +1,15 @@
+﻿using KwikMedical_Server.Models;
+
+namespace KwikMedical_Server.Repositories
+{
+    /// <summary>
+    /// interface is designed for operations related to ambulance records.
+    /// </summary>
+    public interface IAmbulanceRecordRepository
+    {
+        Task<IEnumerable<AmbulanceRecord>> GetAllRecordsAsync();
+        Task<AmbulanceRecord> GetRecordByIdAsync(int id);
+        Task AddRecordAsync(AmbulanceRecord record);
+        Task SaveChangesAsync();
+    }
+}
---
+++ ASP.NET Project/ASP.NET_SourceFiles/Repositories/IPatientRepository.cs
@@ -0,0 +1,18 @@
+﻿using KwikMedical_Server.Models;
+
+namespace KwikMedical_Server.Repositories
+{
+    /// <summary>
+    /// Interface defines a contract for operations related to Patientcs. 
+    /// It includes methods for CRUD (Create, Read, Update, Delete) operations, focusing on patients
+    /// </summary>
+    public interface IPatientRepository
+    {
+        Task<IEnumerable<Patientcs>> GetAllPatientsAsync();
+        Task<Patientcs> GetPatientByIdAsync(int id);
+        Task<Patientcs> GetPatientByChiNumberAsync(string chiNumber);
+        Task AddPatientAsync(Patientcs patient);
+        Task UpdatePatientAsync(Patientcs patient);
+        Task DeletePatientAsync(int id);
+    }
+}
